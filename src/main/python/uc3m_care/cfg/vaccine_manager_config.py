1a5340562d5ecb77165cae40a546cc72943a0419
RuthNavarroCarrasco <98970119+RuthNavarroCarrasco@users.noreply.github.com>
2022-05-05 18:11:01 +0200

    Configuration updates

---
+++ src/main/python/uc3m_care/cfg/vaccine_manager_config.py
@@ -0,0 +1,4 @@
+"""Global constants for finding the path"""
+from pathlib import Path
+JSON_FILES_PATH = str(Path.home()) + "/PycharmProjects/G88.2022.T05.FP/src/JsonFiles/"
+JSON_FILES_RF2_PATH = JSON_FILES_PATH + "/RF2/"
