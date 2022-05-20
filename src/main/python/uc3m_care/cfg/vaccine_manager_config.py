8f48b283e2a1eac2c78d649c0c46ffb0af840231
RuthNavarroCarrasco <98970119+RuthNavarroCarrasco@users.noreply.github.com>
2022-05-20 12:53:23 +0200

    json files added for cancel_appointment test

--- src/main/python/uc3m_care/cfg/vaccine_manager_config.py
+++ src/main/python/uc3m_care/cfg/vaccine_manager_config.py
@@ -2,3 +2,4 @@
 from pathlib import Path
 JSON_FILES_PATH = str(Path.home()) + "/PycharmProjects/G88.2022.T05.FP/src/JsonFiles/"
 JSON_FILES_RF2_PATH = JSON_FILES_PATH + "/RF2/"
+JSON_FILES_RF3_PATH = JSON_FILES_PATH + "/RF3/"
